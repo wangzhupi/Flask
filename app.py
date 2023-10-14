a1456f877b0f59183143bfebe2eab2c9b8dfea97
wangzijian <2422717601@qq.com>
2023-10-14 13:11:42 +0800

    初始化app

--- app.py
+++ app.py
@@ -2,11 +2,15 @@ from flask import Flask, url_for, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/') #直接进到页面
 def index():
     # 基础欢迎界面
     return render_template('index.html',message="Hello world")
 
+@app.route('/login') # 登录路由
+def login():
+    # 处理登录逻辑...
+    return "This is the login page"
 
-if __name__ == '__main__':
-    app.run()
+# if __name__ == '__main__':
+#     app.run()
